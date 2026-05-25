0b59bb9e1cd3c6ff92e57a272c9d8de8030061d1
a113010029 <a113010029@github.com>
2026-05-25 21:28:35 +0800

    feat: rename to 美食爆爆, update home/blog/about content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '今日校園新聞';
-export const SITE_DESCRIPTION = '記錄校園生活、美食、活動的第一手報導';
+export const SITE_TITLE = '🍿 美食爆爆';
+export const SITE_DESCRIPTION = '爆料校園周邊最好吃的美食，每週新鮮報導！';
